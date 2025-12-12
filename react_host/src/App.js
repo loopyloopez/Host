6429439418c3ba7ad1bde4b09dcb78b6d4baca2b
loopyloopez <pin.filipe.l@gmail.com>
2025-12-11 21:04:02 -0600

    added websocket package and reverted bad changes

--- react_host/src/App.js
+++ react_host/src/App.js
@@ -1,13 +1,17 @@
 
 import './App.css';
 import React from 'react';
+
 //import Table from './Components/tables';
 import FrontDesk from './Components/frontDesk';
 import Drows from './Components/Drows';
 import Arows from './Components/Arows';
 import Brows from './Components/Brows';
 import Erows from './Components/Erows';
+
+
 function App() {
+  
   return (
     <div>
       <Erows />
